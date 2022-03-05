1f171474e2c5b4bdaa767adccf6df3c86f85c37e
RAUNAK22092002 <76444444+RAUNAK22092002@users.noreply.github.com>
2022-03-05 19:17:57 +0530

    Add files via upload

---
+++ Books/script.js
@@ -0,0 +1,9 @@
+
+const msg = document.querySelector(".msg");
+
+const btn = document.querySelector(".button");
+btn.addEventListener("click", func);
+
+function func() {
+   alert("The message has been sent")
+}
